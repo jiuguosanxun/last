fce3cfbb41de87b0ab549abbe0e3b819388f497b
jiuguosanxun <116093104+jiuguosanxun@users.noreply.github.com>
2022-10-18 22:38:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var box = Document.getElementById("box");
+var screen = box.children[0];
+var ul = screen.children[0];
+var ol = screen.children[1];
+var lisul= ul.children;
+var imgwid = screen.offsetWidth;
+var arr = box.children[1];
+var arrleft =arr.children[0];
+var arrleft = arr.children[1];
+for(var i=0;i<lisul.length)
